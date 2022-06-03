345c57d57e731e0ffe34933166d3991af82bc1b0
Michael2M <77737524+Michael2M-dot@users.noreply.github.com>
2022-06-03 15:07:47 +0300

    new feature vertical slider

--- 03-slider-sources/index.js
+++ 03-slider-sources/index.js
@@ -2,5 +2,43 @@ const upBtn = document.querySelector('.up-button');
 const downBtn = document.querySelector('.down-button');
 const sidebar = document.querySelector('.sidebar');
 const mainSlide = document.querySelector('.main-slide');
+const container = document.querySelector('.container');
+const slidesCount = mainSlide.querySelectorAll('div').length;
 
-sidebar.style.top = `-${n * 100}vh`
+sidebar.style.top = `-${(slidesCount -1) * 100}vh`;
+
+//переменная счетчик слайдов
+let activeSlideIndex = 0
+
+//функция смены направления слайдов
+function changeSlide(direction) {
+  if(direction === 'up') {
+    activeSlideIndex++;
+    if(activeSlideIndex === slidesCount) {
+      activeSlideIndex = 0;
+    }
+  }
+  if(direction === 'down') {
+    activeSlideIndex--;
+    if(activeSlideIndex <0) {
+      activeSlideIndex = slidesCount -1;
+    }
+  }
+
+  //получаем высоту контейнера
+  const height = container.clientHeight;
+
+  mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+
+  sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
+}
+
+
+// слушатели событий на кнопках
+upBtn.addEventListener('click', () => {
+  changeSlide('up');
+});
+
+downBtn.addEventListener('click', () => {
+  changeSlide('down');
+});
