8e5acd1a77021879e0840305d2182a6e6ad53c0a
Michael2M <77737524+Michael2M-dot@users.noreply.github.com>
2022-06-03 14:29:36 +0300

    added markup for new slides feature

---
+++ 03-slider-sources/index.js
@@ -0,0 +1,6 @@
+const upBtn = document.querySelector('.up-button');
+const downBtn = document.querySelector('.down-button');
+const sidebar = document.querySelector('.sidebar');
+const mainSlide = document.querySelector('.main-slide');
+
+sidebar.style.top = `-${n * 100}vh`
