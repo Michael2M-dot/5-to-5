66d3792e388f8c3e2378be7fcc18ad22bf9ef625
Michael2M <77737524+Michael2M-dot@users.noreply.github.com>
2022-06-05 12:09:59 +0300

    added new feature for aim game

---
+++ 05-aim-game-sources/index.js
@@ -0,0 +1,82 @@
+const startBtn = document.querySelector('#start');
+const screens = document.querySelectorAll('.screen');
+const timeList = document.querySelector('#time-list');
+const timeGame = document.querySelector('#time');
+const board = document.querySelector('#board');
+
+//счетчик времяни для игры
+let time = 0;
+//счетчик очков в игре
+let score = 0;
+
+//начало игры
+function startGame() {
+  setTime(time);
+  createRandomCurcle();
+  setInterval(decreseTime, 1000);
+}
+
+function decreseTime() {
+  if(time === 0){
+    finishGame();
+  } else {
+    let current = --time;
+
+    if(time < 10) {
+      current = `0${time}`;
+    } 
+    setTime(current);
+    }
+}
+
+function setTime(val) {
+  timeGame.innerHTML = `00:${val}`;
+}
+
+//остновка игры
+function finishGame() {
+  board.innerHTML = `<h1>Счет:<span class = "primary">${score}</span></h1>`;
+  timeGame.parentNode.classList.add('hide');
+}
+
+//добавляем элементы в DOM 
+function createRandomCurcle() {
+  const circle = document.createElement('div');
+  const size = getRandomNumber(10, 60);
+  const { width, height } = board.getBoundingClientRect();
+
+  const x = getRandomNumber(0, width - size);
+  const y = getRandomNumber(0, height - size);
+
+  circle.classList.add('circle');
+  circle.style.setProperty('--circleSize', `${size}px`);
+  circle.style.top = `${y}px`;
+  circle.style.left = `${x}px`;
+  board.append(circle);
+}
+
+const getRandomNumber = (min, max) => Math.round(Math.random() * (max - min) + min);
+
+//слушатели событий
+startBtn.addEventListener('click', (e) => {
+  e.preventDefault;
+
+  screens[0].classList.add('up');
+})
+
+timeList.addEventListener('click', (e) => {
+
+  if(e.target.classList.contains('time-btn')) {
+    time = +e.target.getAttribute('data-time');
+    screens[1].classList.add('up');
+    startGame();
+  }
+})
+
+board.addEventListener('click', (e) => {
+  if(e.target.classList.contains('circle')){
+    score++;
+    e.target.remove();
+    createRandomCurcle();
+  }
+})
